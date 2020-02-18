6690ab35769fb1b2bbb42347269d5aab55dfe271
Justin Schroeder <justin@wearebraid.com>
2020-02-18 13:08:40 -0500

    Removes the original source code

--- src~v1/errors.js
+++
@@ -1,7 +0,0 @@
-export default {
-  required: ({label, value}) => `${label} is required`,
-  email: ({label, value}) => `${label} is invalid.`,
-  confirmed: ({label, value}) => `${label} does not match the confirmation field.`,
-  number: ({label, value}) => `${label} is not a number`,
-  default: ({label, value}) => `This field is invalid.`
-}
