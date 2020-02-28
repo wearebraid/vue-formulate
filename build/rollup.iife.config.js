9775e147700a4836f06ef1f56ae8ef1fafad9c6a
Justin Schroeder <justin@wearebraid.com>
2020-02-28 10:46:08 -0500

    Cleans up dependencies and bundles requirements in browser minified version

--- build/rollup.iife.config.js
+++ build/rollup.iife.config.js
@@ -1,24 +1,29 @@
-import autoExternal from 'rollup-plugin-auto-external'
+import resolve from '@rollup/plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs' // Convert CommonJS modules to ES6
 import vue from 'rollup-plugin-vue' // Handle .vue SFC files
 import buble from 'rollup-plugin-buble' // Transpile/polyfill with reasonable browser support
+import internal from 'rollup-plugin-internal'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/Formulate.js', // Path relative to package.json
   output: {
     name: 'Formulate',
-    exports: 'named'
-    // globals: {
-    //   'is-plain-object': 'isPlainObject',
-    //   'nanoid': 'nanoid',
-    //   'is-url': 'isUrl'
-    // }
+    exports: 'named',
+    globals: {
+      'is-plain-object': 'isPlainObject',
+      'nanoid': 'nanoid',
+      'is-url': 'isUrl',
+      'crypto': 'crypto'
+    }
   },
-  // external: ['is-plain-object', 'nanoid', 'is-url'],
   plugins: [
+    resolve({
+      browser: true,
+      preferBuiltins: false
+    }),
     commonjs(),
-    autoExternal(),
+    internal(['is-plain-object', 'nanoid', 'is-url', 'crypto']),
     vue({
       css: true, // Dynamically inject css as a <style> tag
       compileTemplate: true // Explicitly convert template to render function
