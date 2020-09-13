8c45e52892622c63025f0231d4ebe33dd84a300e
Justin Schroeder <justin@wearebraid.com>
2020-09-12 23:53:18 -0400

    Restores specimen sheet

--- examples/specimens/SpecimenFile.vue
+++ examples/specimens/SpecimenFile.vue
@@ -1,34 +1,21 @@
 <template>
-  <FormulateForm @submit="submit">
-    <div class="specimens specimens--text">
-      <h2>File classification</h2>
-      <div class="specimen specimen">
-        <h3>Text</h3>
-        <FormulateInput
-          label="Upload a file"
-          type="file"
-          help="Select any file to upload"
-        />
-      </div>
-      <div class="specimen">
-        <h3>Image</h3>
-        <FormulateInput
-          label="What do you look like?"
-          type="image"
-          help="Select a picture to upload."
-        />
-      </div>
+  <div class="specimens specimens--text">
+    <h2>File classification</h2>
+    <div class="specimen specimen">
+      <h3>Text</h3>
+      <FormulateInput
+        label="Upload a file"
+        type="file"
+        help="Select any file to upload"
+      />
     </div>
-    <FormulateInput type="submit" />
-  </FormulateForm>
+    <div class="specimen">
+      <h3>Image</h3>
+      <FormulateInput
+        label="What do you look like?"
+        type="image"
+        help="Select a picture to upload."
+      />
+    </div>
+  </div>
 </template>
-
-<script>
-export default {
-  methods: {
-    submit (data) {
-      console.log(data)
-    }
-  }
-}
-</script>
