09226b9d4dc5ce2ee08b97fa197aa10236f4bba0
Andrew Boyd <andrew@wearebraid.com>
2020-12-23 19:36:52 -0500

    updates nuxt module to expect external config file

--- dist/nuxt/plugin.js
+++ dist/nuxt/plugin.js
@@ -1,5 +1,9 @@
-/* eslint-disable */
 import Vue from 'vue'
 import VueFormulate from '@braid/vue-formulate'
+<% if (options.configPath) { %>
+import options from '<%= options.configPath %>'
+<% } else { %>
+const options = {}
+<% } %>
 
-Vue.use(VueFormulate, <%= serialize(options) %>)
+Vue.use(VueFormulate, options)
--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import VueFormulate from '@braid/vue-formulate'
+<% if (options.configPath) { %>
+import options from '<%= options.configPath %>'
+<% } else { %>
+const options = {}
+<% } %>
 
-Vue.use(VueFormulate, <%= serialize(options) %>)
+Vue.use(VueFormulate, options)
